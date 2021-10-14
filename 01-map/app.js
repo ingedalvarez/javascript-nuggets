515951f269974191087edd92ce5133e94817de64
ingedalvarez <ingedalvarez@gmail.com>
2021-10-14 11:57:50 -0500

    Insercion de valores dentro del index.html a traves de app.js fin de las modificaciones.

--- 01-map/app.js
+++ 01-map/app.js
@@ -39,7 +39,7 @@ const newPeople = people.map((item) => {
 });
 console.log(newPeople);
 
-const names = people.map((person) => `<h2>${person.name}</h2>`);
+const names = people.map((person) => ``);
 const result = document.querySelector('#result');
 
 result.innerHTML = names.join('');
@@ -53,8 +53,40 @@ const people = [
   { name: 'Isabella', age: 2, possition: 'CEO' },
 ];
 
-const ages = people.map((person) => {
-  console.log(person);
-  return console.log('hello world');
+const ages1 = people.map((person) => {
+  return 'hello world';
 });
+console.log(ages1);
+
+/*
+sobre un array map aplica el return del callback function a cada uno de los items del array
+*/
+const ages = people.map((person) => {
+  return person.age;
+});
+
 console.log(ages);
+
+// se pueden aplicar operaciones sobre cada uno de los items del array.
+const agesPlus2 = people.map((person) => {
+  return person.age + 2;
+});
+
+console.log(agesPlus2);
+
+// en este ejemplo utilizamos map para retornar un objeto a partir de cada item del array.
+const newPeople = people.map((item) => {
+  return {
+    nameUperCase: item.name.toUpperCase(),
+    utilTime: 70 - item.age,
+  };
+});
+
+console.log(newPeople);
+
+const names = people.map((person) => {
+  return `<h2>${person.name} : ${person.age}</h2>`;
+});
+
+const result = document.querySelector('#result');
+result.innerHTML = names.join('');
