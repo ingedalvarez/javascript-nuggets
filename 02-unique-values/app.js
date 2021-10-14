930a28dbd2fa5a301fd4404d545797a1d4302476
ingedalvarez <ingedalvarez@gmail.com>
2021-10-14 14:14:45 -0500

    desarrollo del tutorial, insercion de comentarios

--- 02-unique-values/app.js
+++ 02-unique-values/app.js
@@ -30,7 +30,29 @@ const menu = [
 // new Set - narrow down
 // ['all',...] - turn it back to array
 
+/* const categories = ['all', ...new Set(menu.map((item) => item.category))];
+const result = document.querySelector('.result');
+result.innerHTML = categories
+  .map((category) => {
+    return `<button>${category}</button>`;
+  })
+  .join(''); */
+
+const categories1 = menu.map((item) => item.category); //Array con elementos repetidos.
+const categoriesSet = new Set(categories1); //Objeto con elementos unicos.
+const categoriesSetArray = [...categoriesSet]; //Array con elementos unicos
+
+console.log(categories1);
+console.log(categoriesSet);
+console.log(categoriesSetArray);
+
+// lo mismo en una sola linea
+
 const categories = ['all', ...new Set(menu.map((item) => item.category))];
+console.log(categories);
+
+//seleccionamos la class .result del div donde queremos agregar los valores.
+
 const result = document.querySelector('.result');
 result.innerHTML = categories
   .map((category) => {
