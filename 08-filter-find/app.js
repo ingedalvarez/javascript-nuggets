c19f03bdc6462c02b5cd1029a7bbb29fe09ee0b9
ingedalvarez <ingedalvarez@gmail.com>
2021-11-22 09:10:46 -0500

    Nuevo array y ejemplos filter and find

--- 08-filter-find/app.js
+++ 08-filter-find/app.js
@@ -36,3 +36,63 @@ console.log(anna);
 
 console.log(peter.position);
 console.log(anna[0].position);
+
+// ======= new example =======
+
+const employee = [
+  {
+    name: 'Bevis Martin',
+    country: 'Brazil',
+    phone: '1-131-967-4145',
+    age: 49,
+    language: 'english',
+  },
+  {
+    name: 'Kyra Murray',
+    country: 'Germany',
+    phone: '1-885-776-7447',
+    age: 37,
+    language: 'spanish',
+  },
+  {
+    name: 'Shad Suarez',
+    country: 'Chile',
+    phone: '(321) 841-2862',
+    age: 38,
+    language: 'english',
+  },
+  {
+    name: 'Salvador Wilkinson',
+    country: 'Mexico',
+    phone: '1-823-125-0294',
+    age: 25,
+    language: 'spanish',
+  },
+  {
+    name: 'Mufutau Crawford',
+    country: 'Canada',
+    phone: '1-445-701-4111',
+    age: 19,
+    language: 'spanish',
+  },
+];
+
+const latino = employee.filter((person) => {
+  return person.language === 'spanish';
+});
+
+console.log(latino);
+console.log(latino.length);
+
+const mexican = employee.find((person) => {
+  return person.country === 'Mexico';
+});
+
+console.log(mexican.name);
+
+const olders = employee.filter((person) => {
+  return person.age >= 37;
+});
+
+console.log(olders);
+console.log(olders.length);
